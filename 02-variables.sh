39bd3fe31d5d1b2e4f2adb0f54dff03a683a2abf
Aniliky <anilmalela9@gmail.com>
2023-12-29 22:29:14 +0530

    added 3rd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 
 person1=ramesh
 person2=suresh
-echo"$person1:hai $person2"
-echo"$person2:Hello $person1"
-echo"$person1:how are you $person2"
-echo"$person2:i am fine thankyou $person1"
+echo "$person1:hai $person2"
+echo "$person2:Hello $person1"
+echo "$person1:how are you $person2"
+echo "$person2:i am fine thankyou $person1"
