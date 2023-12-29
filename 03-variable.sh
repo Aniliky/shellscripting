603feecfa7958db90020db055c61a74cc33df3ef
Aniliky <anilmalela9@gmail.com>
2023-12-29 22:31:45 +0530

    added 3rd script

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:hai $person2"
+echo "$person2:Hello $person1"
+echo "$person1:how are you $person2"
+echo "$person2:i am fine thankyou $person1"
