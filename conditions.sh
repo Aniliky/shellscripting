a6e6fd6bd818a44bf4d6a0995a6d2730cd071a29
Aniliky <anilmalela9@gmail.com>
2023-12-30 11:02:53 +0530

    3rd commit

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ number1=$1
 
 if [$number1 -gt 25] 
 then
-echo "given number is greater than 25"
+    echo "given number is greater than 25"
 else
-echo "given number is less than 25"
+    echo "given number is less than 25"
 fi
